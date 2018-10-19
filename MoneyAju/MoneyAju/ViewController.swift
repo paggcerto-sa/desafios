4824bcc5a1fc1c3354525411fa642501a7f7d6f3
Vitor Costa <vitu86@gmail.com>
2018-10-19 20:13:24 -0300

    Initial commit with Cocoa Pods and gitignore

---
+++ MoneyAju/MoneyAju/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyAju
+//
+//  Created by Vitor Costa on 19/10/18.
+//  Copyright © 2018 Vitor Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MoneyAju/Pods/Target Support Files/Pods-MoneyAju/Pods-MoneyAju-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoneyAju : NSObject
+@end
+@implementation PodsDummy_Pods_MoneyAju
+@end
